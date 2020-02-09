b522cdd2e892e1e29e3419140e249aea55b3742a
SymphonyOfTranquility <mysechko01@gmail.com>
2020-02-09 12:56:24 +0200

    Task1 works

--- LabRasp1/Task1/src/sample/SliderThread.java
+++ LabRasp1/Task1/src/sample/SliderThread.java
@@ -2,20 +2,39 @@ package sample;
 
 import javafx.scene.control.Slider;
 
+import static java.lang.Integer.min;
+import static java.lang.Integer.max;
+
 public class SliderThread extends Thread{
     private Slider slider;
     private int value;
+    private int step;
 
-    SliderThread(Slider slider, int value)
+    SliderThread(Slider slider, int value, int step)
     {
         this.slider = slider;
         this.value = value;
+        this.step = step;
     }
 
     @Override
     public void run()
     {
-        while (!Thread.interrupted())
-            slider.setValue(value);
+        while (!Thread.interrupted()){
+
+            synchronized (slider) {
+
+                int currentValue = (int) slider.getValue();
+                if (currentValue <= 90 && value == 90)
+                    slider.setValue(min(currentValue + step, 90));
+                if (currentValue >= 10 && value == 10)
+                    slider.setValue(max(currentValue + step, 10));
+                try {
+                    Thread.sleep(5);
+                } catch (InterruptedException e) {
+                    Thread.currentThread().interrupt();
+                }
+            }
+        }
     }
 }
