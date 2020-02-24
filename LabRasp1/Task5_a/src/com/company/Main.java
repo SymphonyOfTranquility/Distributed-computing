002fb9fb44d29bbfd9e2d8370721f86e1539ada7
SymphonyOfTranquility <mysechko01@gmail.com>
2020-02-25 00:09:35 +0200

    done task 5 a

--- LabRasp1/Task5_a/src/com/company/Main.java
+++ LabRasp1/Task5_a/src/com/company/Main.java
@@ -4,5 +4,8 @@ public class Main {
 
     public static void main(String[] args) {
 	// write your code here
+        Recruits recruits = new Recruits(200);
+        recruits.orderRecruits();
+        recruits.endRecruits();
     }
 }
